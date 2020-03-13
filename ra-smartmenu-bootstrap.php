83fac3e7e25ad922c1b42188a2eec5bd25d5ae4c
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-03-13 08:39:15 +0800

    Filter nav_menu_link_attributes, added comments

--- ra-smartmenu-bootstrap.php
+++ ra-smartmenu-bootstrap.php
@@ -16,9 +16,14 @@ defined( 'ABSPATH' ) or die( esc_html_e( 'You do not have an access to this real
 
 class RA_Smartmenu_Bootstrap {
   public function __construct() {
-    add_action( 'wp_enqueue_scripts', array( &$this, 'rasb_enqueue_scripts' ) );
+    // Enqueue required CSS and JS files
+    add_action( 'wp_enqueue_scripts', array( $this, 'rasb_enqueue_scripts' ) );
+    
+    // Filter navigation menu link attribute
+    add_filter( 'nav_menu_link_attributes', array( $this, 'rasb_nav_menu_link_attributes' ), 99, 4 );
   }
 
+  // Function to enqueue scripts
   public function rasb_enqueue_scripts() {
     $plugin_version = '1.0';
 
@@ -34,6 +39,16 @@ class RA_Smartmenu_Bootstrap {
       wp_register_script( 'rasb-smartmenu-bootstrap-js', plugin_dir_url( __FILE__ ) . 'public/js/jquery.smartmenus.bootstrap-4.min.js', array( 'jquery', 'rasb-smartmenu-js' ), $plugin_version, true );
       wp_enqueue_script( 'rasb-smartmenu-bootstrap-js' );
     }
+  }
+
+  // Function to filter nav_menu_link_attributes
+  public function rasb_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
+    // Bring back default menu link URL replaced by WP Bootstrap Navwalker
+    if ( $args->has_children && 0 === $depth && $args->depth > 1 ) {
+      $atts['href'] = ! empty( $item->url ) ? $item->url : '';
+    }
+
+    return $atts;
   }
 }
 
